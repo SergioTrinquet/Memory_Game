31f417e5f44eff58fcb0ef362e594b792b5e822c
SergioTrinquet <serge.trinquet@gmail.com>
2022-06-14 14:39:17 +0200

    Ajout promesses pour gérer timing qd enchainement de msgs (msgs plus tronqués)

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -11,7 +11,7 @@
 
 <script setup>
     import { useStore } from 'vuex'
-    import { defineProps, ref, computed, watch } from 'vue'
+    import { defineProps, ref, watch } from 'vue'
     import anime from 'animejs/lib/anime.es.js';
 
     const store = useStore()
@@ -45,75 +45,122 @@
             }
         } 
     })
-    const msg = computed(() => props.content)
+
     let localMsg = ref("");
 
+    watch( 
+        () => props.content, 
+        (val) => {
+            console.log("watch sur 'props.content'", JSON.stringify(val)); //TEST
+            // displayMessage(val)
+            queueMessages(val)
+        }
+    )
 
-    watch(msg, (val) => {
-        console.log("watch sur 'msg'", JSON.stringify(val)); //TEST
-        displayMessage(val)
-    })
+    /////////////
+    // Pour chainer les messages sans que e dernier appelé ne 'affiche avant la fin du précédent
+    //let countCalls = 0;
+    let resolved = null;
+    let P = null;
+    async function queueMessages(val) {
+        // V1 : Fonctionne !!!
+        /* if(countCalls > 0) {
+            await P;
+            console.log("La promesse précédente est terminée....");
+        }
+        countCalls += 1; console.log("1 - countCalls", countCalls); //TEST
+        resolved = await displayMessage(val)
+        if(resolved == 'resolved') {
+            countCalls -= 1;    console.log("2 - countCalls", countCalls); //TEST
+            resolved = null
+        } */
+
+        // V2 : Fonctionne !!
+        if(resolved !== null) {
+            await P;
+            console.log("La promesse précédente est terminée...."); // TEST
+        }
+        resolved = "pending"
+        resolved = await displayMessage(val)
+        if(resolved == 'resolved') resolved = null
+    }
+    /////////////
+
+    let tl;///
+    async function displayMessage(val) {
+        //console.log("VALEUR DE VAL =>", val); //TEST
+        P = new Promise(resolve => { ////
+
+            // Anime.js
+            //let tl = anime.timeline({
+             tl = anime.timeline({
+                targets: '.contentMessage',
+                begin: (anim) => console.log("Animation commencée", anim.began), // TEST
+                complete: (anim) => { 
+                    console.log("Animation terminée", anim.completed); // TEST
+                    resolve('resolved'); // Fin de la promesse 
+                }                        
+            });
+            tl.restart();
+
+            // Si val n'est pas un tableau, il en devient un
+            if(!Array.isArray(val)) {
+                val = [val];
+            } 
+
+            val.forEach(v => {  
+                if(!!v.animationName !== false) { // Si animation présente...
+                    // On check si l'anim passée existe...
+                    if(!store.getters.isAnimationNameValid(v.animationName)) {
+                        localMsg.value = v.text;
+                        console.error("Libellé d'animation de texte invalide !");
+                        return
+                    }
+
+                    const animationMsg = store.getters.getMessageAnimationByName(v.animationName);
+                    //console.table("animations", animationMsg.animations); //TEST
+
+                    tl
+                        .add({
+                            duration: 1,
+                            begin: (anim) => {
+                                console.log("Animation ds la boucle commencée", anim.began); //TEST
+                                localMsg.value = v.text;
+                            }
+                        });
+
+                    //TEST
+                    /* tl.add({ scale: [0, 1], opacity: 1, duration: 300, easing: "easeInQuad" }, "-=300")
+                    .add({ scale: 4, opacity: 1, duration: 300, easing: "easeOutQuad" })
+                    .add({ opacity: [1, 0], duration: 200, easing: "easeOutQuad" }); */
+                    //FIN TEST
+
+                    animationMsg.animations.forEach(anim => tl.add(anim) );
+
+                    tl.finished.then(() => { 
+                        console.log("Animation finished !!!"); //TEST
+                        localMsg.value = "";
+                        document.querySelector('.contentMessage').removeAttribute('style');
+                    })
+                } /* else {
+                    throw "Libellé d'animation de texte pas présente"
+                } */
+            })
 
 
-    function displayMessage(val) {
-        console.log("VALEUR DE VAL =>", val); //TEST
 
-        // Anime.js
-        let tl = anime.timeline({
-            targets: '.contentMessage',
-            begin: (anim) => console.log("Animation commencée", anim.began), // TEST
-            complete: (anim) => console.log("Animation terminée", anim.completed) // TEST
-        });
-        tl.restart();
-
-        // Si val n'est pas un tableau, il en devient un
-        if(!Array.isArray(val)) {
-            val = [val];
-        } 
-
-        val.forEach(v => {  
-            if(!!v.animationName !== false) { // Si animation présente...
-                // On check si l'anim passée existe...
-                if(!store.getters.isAnimationNameValid(v.animationName)) {
-                    localMsg.value = v.text;
-                    console.error("Libellé d'animation de texte invalide !");
-                    return
-                }
-
-                const animationMsg = store.getters.getMessageAnimationByName(v.animationName);
-                console.table("animations", animationMsg.animations); //TEST
-
-                tl
-                    .add({
-                        duration: 1,
-                        begin: (anim) => {
-                            console.log("Animation ds la boucle commencée", anim.began); //TEST
-                            localMsg.value = v.text;
-                        }
-                    });
-
-                //TEST
-                /* tl.add({ scale: [0, 1], opacity: 1, duration: 300, easing: "easeInQuad" }, "-=300")
-                .add({ scale: 4, opacity: 1, duration: 300, easing: "easeOutQuad" })
-                .add({ opacity: [1, 0], duration: 200, easing: "easeOutQuad" }); */
-                //FIN TEST
-
-                animationMsg.animations.forEach(anim => tl.add(anim) );
-
-                tl.finished.then(() => { 
-                    //console.log("Animation finished !!!"); //TEST
-                    clearMsg();
-                    document.querySelector('.contentMessage').removeAttribute('style');
-                })
-            } /* else {
-                throw "Libellé d'animation de texte pas présente"
-            } */
-        })
+        }); ////
+        return P; ////
 
     }
 
-
-    function clearMsg() { localMsg.value = "" }
+    // Appelé qd click sur fond message pour le faire disparaitre
+    function clearMsg() { 
+        localMsg.value = ""; 
+        // On va directement à la fin de l'animation en cours, ce qui entraine la fin de la promesse :'resolve' appelé dès la fin de l'anim. que l'on vient d'écourter
+        // Cela permet de passer au message suivant s'il y en a un, sans attendre la fin de la promesse
+        tl.seek(tl.duration); 
+    }
 </script>
 
 <style scoped>
