3579dd0144fc9bba518f776b6989b92d63e5d436
SergioTrinquet <serge.trinquet@gmail.com>
2022-06-22 17:48:28 +0200

    Suite test mobile

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@ html {
 }
 body {
   border: solid 2px rgb(255, 0, 0); 
-  min-height: 100vh;
+  /* min-height: 100vh; */ height: 100vh;
   min-height: -webkit-fill-available;
 }
 /* FIN Ajout le 22/06/2022 */
