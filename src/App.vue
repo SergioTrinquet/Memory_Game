a2b8b2e09eb244228054ccdc2d5358d9b1186384
SergioTrinquet <serge.trinquet@gmail.com>
2022-06-22 22:43:10 +0200

    Suite test mobile

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@ body {
 /* Fonctionne !! A garder */
 /* html, body {
   height: 100%;
-  overflow: hidden; /* Empeche de reloader qd pull vers le bas sur mobile */
+  overflow: hidden; /* Empeche de reloader qd pull vers le bas sur mobile */ /*
 } */
 /* FIN */
 
