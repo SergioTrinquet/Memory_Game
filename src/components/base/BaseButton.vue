522d8b59124a02d966c7bc21c6f7a77bc60aac17
SergioTrinquet <serge.trinquet@gmail.com>
2022-06-28 17:30:47 +0200

    Composant Bouton

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,106 @@
+<template>
+  <button class="bt" :class="CSS">
+      <span class="libelle">
+          <slot />
+      </span>
+      <span class="bg"></span>
+  </button>
+</template>
+
+<script setup>
+    import { defineProps, computed } from 'vue'
+
+    const props = defineProps({
+        color: {
+            type: String,
+            required: false
+        },
+        outline: {
+            type: String,
+            required: false
+        },
+        hover: {
+            type:  Object,
+            required: false,
+            validator(value) {
+                const val = Object.prototype.toString.call(value);
+                // Si objet...
+                if(val === '[object Object]') {
+                    if(!('color' in value && 'backgroundColor' in value)) {
+                        console.error("OBJET : Il manque la ou les propriétés 'color' et 'backgroundColor' à la prop passée !!", value)
+                        return false
+                    } else {
+                        return true
+                    }
+                } else {
+                    return false
+                }
+            }
+        },
+        rounded: {
+            type: Boolean,
+            required: false 
+        },
+        fontSize: {
+            type: String,
+            required: false 
+        }
+    })
+
+    const CSS = computed(() => {
+        let classes = "";
+        if(props.outline) classes += "outline "
+        if(props.hover) classes += "hover "
+        if(props.rounded) classes += "rounded "
+        return classes
+    })
+
+    const CSSfontSize = computed(() => (typeof props.fontSize == 'undefined') ? "initial" : props.fontSize)
+</script>
+
+<style scoped>
+    button.bt {
+        padding: 2vmin 3vmin;
+        transition: all 0.3s ease-in-out;
+        cursor: pointer;
+        border-style: solid;
+        border-radius: 5px;
+        border-width: 0;
+        position: relative;
+        overflow: hidden;
+        display: flex; 
+        align-items: center; 
+        justify-content: center;
+    }
+    button.bt.outline {
+        border-width: 4px;
+        border-color: v-bind('props.outline');
+        color: v-bind('props.outline');
+        background-color: transparent;
+    }
+    button.bt.rounded {
+        border-radius: 100vh;
+    }
+    button > .libelle { 
+        /* position: absolute; */ 
+        z-index:2;
+        font-size: v-bind('CSSfontSize');
+    }
+    button.bt > .bg {
+        content: "";
+        width: 100%;
+        height: 100%;
+        background-color: v-bind('props.hover.backgroundColor');
+        top: 0;
+        left: -100%;
+        position: absolute;
+        z-index: 0;
+        transition: left 0.3s ease-in-out;
+    }
+    button.bt:hover {
+        color: v-bind('props.hover.color');
+    }
+    button.bt:hover > .bg {
+        left: 0%;
+    }
+</style>
