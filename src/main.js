a1d5240e9e409a7344fdd9d182ab738b80b6e79e
SergioTrinquet <serge.trinquet@gmail.com>
2022-07-06 16:28:51 +0200

    Suppression icones font-awesome

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import router from './router'
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
 /* import specific icons */
-import { faArrowRight, faCircleArrowRight, faCircleArrowLeft, faCheck, faBars, faXmark, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faArrowLeft, faCheck, faBars, faXmark, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
-library.add(faArrowRight, faCircleArrowRight, faCircleArrowLeft, faCheck, faBars, faXmark, faExclamationCircle)
+library.add(faArrowRight, faArrowLeft, faCheck, faBars, faXmark, faExclamationCircle)
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
