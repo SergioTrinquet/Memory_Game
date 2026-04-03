d52c7aa2bac1e980d60c85598d83361c71313dc5
SergioTrinquet <serge.trinquet@gmail.com>
2026-04-03 17:57:56 +0200

    refactor: Rename and reorganize Intro component structure for clarity

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="content">   
+    <div class="content-intro">   
 
         <div class="score" :class="animationScore ? 'anim' : ''">+1</div>
         <div class="wrapper-card first">
@@ -15,9 +15,9 @@
             />
         </div>
 
-        <div class="carre-test"></div><!-- TEST -->
-        <div class="carre"></div>
-        <div class="content-txt">
+        <div class="square first"></div>
+        <div class="square last"></div>
+        <div class="content-intro-txt">
             <div class="txt-intro">Bienvenue dans le jeu<br />du Memory !</div>
             <BaseButton 
                 @click="goToSettings"
@@ -27,6 +27,13 @@
             >
                 Jouer une partie !
             </BaseButton>
+            <!-- <BaseButton 
+                @click="goToSettings"
+                font-size="max(22px, 4vmin)"
+                rounded
+            >
+                Jouer une partie !
+            </BaseButton> -->
         </div>
     </div>
 </template>
@@ -35,21 +42,13 @@
     import BaseButton from '@/components/base/BaseButton.vue'
     import Card from '@/components/Card.vue'
     import { onMounted, ref } from 'vue'  
-    /* import anime from 'animejs/lib/anime.es.js'; */
-
     import { useRouter } from 'vue-router'
 
-    const router = useRouter();
-
-    function goToSettings() {
-        router.push({ name: 'parametres' }) // Redirection vers page 'Settings'
-    }
-
-    // Gestion cartes qui tournent
     let i = 0
+    const router = useRouter();
     const cardState1 = ref(0)
     const cardState2 = ref(0)
-    const idxImgs = [[1, 5], [3, 3], [2, 4], [3, 5], [10, 10], [13, 14]] // Indexs pris au hasard des images de fruits
+    const idxImgs = [[1, 5], [3, 3], [2, 4], [3, 5], [10, 10], [13, 14]]
     const flattenIdxImgs = idxImgs.flat()
     const idxImgCard1 = ref(1)
     const idxImgCard2 = ref(1)
@@ -58,11 +57,14 @@
     const delayFlipSecondCard = interval / 2.75 //Soit 2000
     const delayFlipBothCards = delayFlipSecondCard * 2
 
-    function flipCards() {
-        // On retourne la 1ere carte (celle de gauche)
-        flipFrontCard(idxImgCard1, cardState1)
+    function goToSettings() {
+        router.push({ name: 'parametres' }) // Redirection vers page 'Settings'
+    }
 
-        // On retourne la 2eme carte (celle de droite) 2 sec. plus tard...
+    function flipCards() {
+        flipFrontCard(idxImgCard1, cardState1) // flip 1ere carte
+
+        // flip 2eme carte après délai...
         setTimeout(() => {
             flipFrontCard(idxImgCard2, cardState2)
 
@@ -71,7 +73,7 @@
             if(idxImgCard1.value === idxImgCard2.value) animationScore.value = true
         }, delayFlipSecondCard)
 
-        // On retourne les 2 cartes en même temps pour cacher le contenu au bout de 4 sec.
+        // flip des 2 cartes en même temps pour cacher le contenu au bout de 4 sec.
         setTimeout(() => {
             cardState1.value = 0
             cardState2.value = 0
@@ -90,17 +92,12 @@
         // Animation cartes
         flipCards()
         setInterval(flipCards, interval)
-
-        /* anime({
-            targets: '.txt-intro',
-            translateX: 500,
-            duration: 3000
-        }) */
     })
 </script>
 
 <style scoped>
-.content {
+.content-intro {
+    --top-position-el-pg-intro: -56vmin;
     height: 100%;
     display: flex;
     flex-direction: column;
@@ -109,40 +106,35 @@
     font-size: 5vmin;
     padding: 7vmax 3vmax 3vmax 3vmax;
 }
-.carre {
+
+.square {
     position: fixed;
-    z-index: 1;
-    opacity: 0.15;
     background-color: #fff;
-    width: max(150px, 40vmin);
     aspect-ratio: 1 / 1;
+    width: max(150px, 40vmin);
     border-radius: max(10px, 2vmin);
     transform: rotate(45deg);
-    animation:  4s ease infinite squareRotation;
+    &.first {
+        z-index: 0;
+        opacity: 0.05;
+        animation:  4s ease 2s infinite squareRotationInverse;
+    }
+    &.last {
+        z-index: 1;
+        opacity: 0.15;
+        animation:  4s ease infinite squareRotation;
+    }
 }
 @keyframes squareRotation {
     80% { transform: rotate(45deg); }
     100% { transform: rotate(135deg); }
-}
-
-.carre-test {
-    content: "";
-    position: fixed;
-    z-index: 0;
-    opacity: 0.05;
-    background-color: #fff;
-    width: 40vmin;
-    aspect-ratio: 1 / 1;
-    border-radius: max(10px, 2vmin);
-    transform: rotate(45deg);
-    animation:  4s ease 2s infinite squareRotationInverse;
 }
 @keyframes squareRotationInverse {
     80% { transform: rotate(45deg); }
     100% { transform: rotate(-45deg); }
 }
 
-.content-txt {
+.content-intro-txt {
     position: absolute;
     z-index: 2;
     margin-top: 7vmin;
@@ -161,6 +153,12 @@ button {
     width: 70%;
     box-shadow: 0 0.5vmin 1vmin rgba(0,0,0,0.3);
     white-space: nowrap;
+
+    /* &:hover {
+        background-color: yellow;
+        background-color: red;
+        color: var(--color-primary-dark-3);
+    } */
 }
 :deep(.libelle) {
     transform: translateY(-2px);
@@ -174,15 +172,14 @@ button {
     margin: var(--top-position-el-pg-intro) 0 0 -14vmin;
     width: max(60px, 16vmin);
     height: max(60px, 16vmin);
+    &.first {
+        transform: rotate(-15deg);
+    }
+    &.last {
+        transform: rotate(15deg);
+        margin-left: max(60px, 16vmin);
+    }
 }
-.wrapper-card.first {
-    transform: rotate(-15deg);
- }
- .wrapper-card.last {
-    transform: rotate(15deg);
-    margin-left: max(60px, 16vmin);
- }
-
 :deep(.flip-card-inner) {  
     box-shadow: 0 1vmin 2vmin rgba(0,0,0,0.4); /* SURGHARGE */
 }
@@ -197,9 +194,9 @@ button {
     font-family: 'Fredoka', sans-serif;
     font-weight: 500;
     opacity: 0;
-}
-.score.anim {
-    animation: fadeInUp 1s ease-out 1.5s backwards;
+    &.score.anim {
+        animation: fadeInUp 1s ease-out 1.5s backwards;
+    }
 }
 @keyframes fadeInUp {
     0% { opacity: 1; transform: scale(0) }
